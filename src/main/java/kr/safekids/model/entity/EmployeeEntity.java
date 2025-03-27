05e0158ecbd4aedf5f788038431969e0a3a52990
leebinibini <qpdjvn0601@naver.com>
2025-03-27 17:47:29 +0900

    use barnch creat

--- src/main/java/kr/safekids/model/entity/EmployeeEntity.java
+++ src/main/java/kr/safekids/model/entity/EmployeeEntity.java
@@ -1,7 +1,6 @@
 package kr.safekids.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
